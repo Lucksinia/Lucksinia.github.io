fe22cae2bfe7181f89131c794f5c68d14440ccb7
Lucksinia <denni334334@gmail.com>
2023-12-04 23:00:54 +0300

    started ssg

---
+++ ssg.py
@@ -0,0 +1,26 @@
+import jinja2, markdown2, tomllib, re, pathlib
+
+
+def load_config(cfg_str):
+    return tomllib.loads(cfg_str)
+
+
+def load_content():
+    pass
+
+
+def load_templates():
+    pass
+
+
+def app():
+    cfg_str = pathlib.Path().cwd() / "config.toml"
+    print(cfg_str)
+    # config = load_config(cfg_str)
+    content = load_content()
+    templates = load_templates()
+
+    # render(config, content, templates)
+
+
+app()
---
+++ test/test.py
@@ -0,0 +1,6 @@
+import markdown2, pathlib
+
+path_to_test = pathlib.Path().cwd() / "content" / "posts" / "test-post.md"
+path_to_output = pathlib.Path().cwd() / "test" / "out.html"
+html = markdown2.markdown(str(path_to_test.open().read()), use_file_vars=True)
+path_to_output.write_text(str(html))
