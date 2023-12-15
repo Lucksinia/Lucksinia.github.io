516af064a82032c02f65a82e17ba9773251413bd
Lucksinia <denni334334@gmail.com>
2023-12-15 16:57:27 +0300

    fixed prepper indexing for posts

--- ssg.py
+++ ssg.py
@@ -1,5 +1,8 @@
-import jinja2, markdown2, tomllib, re, pathlib
+import jinja2, markdown2, tomllib, re, pathlib, pprint
 from datetime import datetime
+import shutil
+
+pp = pprint.PrettyPrinter(depth=2)
 
 
 def load_config(cfg_file):
@@ -8,7 +11,9 @@ def load_config(cfg_file):
 
 
 def load_content(content_dir):
+    # list of posts
     posts = []
+    item = {}
     path = pathlib.Path.cwd()
     for file in path.glob(f"{content_dir}/*.md"):
         content = markdown2.markdown(
@@ -21,22 +26,36 @@ def load_content(content_dir):
         content.metadata[
             "url"
         ] = f"/{content.metadata['date'].year}/{content.metadata['slug']}"
-        print(content.metadata)
-        posts.append(content)
-    posts.sort(key=lambda x: x.metadata["date"], reverse=True)
+        # adding diferent dict because markdown2 is funky as hell and not allows posts into list
+        # praised be standart dict impl as Ordered.
+        item = content.metadata
+        item["post"] = content
+        posts.append(item)
+    posts.sort(key=lambda x: x["date"], reverse=True)
     return posts
 
 
-def load_templates():
-    pass
+def load_templates(template_path):
+    fs_loader = jinja2.FileSystemLoader(template_path)
+    return jinja2.Environment(loader=fs_loader)
+
+
+def create_render(config, content, env, output_dir):
+    out_p = pathlib.Path(output_dir)
+    if out_p.exists():
+        shutil.rmtree(output_dir)
+    out_p.mkdir()
+
+    # index page for posts
 
 
 def app():
     config = load_config("config.toml")
-    content = load_content("content/posts")
-    # templates = load_templates()
-
-    # render(config, content, templates)
+    # * pp.pprint(config)
+    content = {"posts": load_content("content/posts")}
+    # * pp.pprint(content)
+    environment = load_templates("layout")
+    create_render(config, content, environment, "public")
 
 
 app()
